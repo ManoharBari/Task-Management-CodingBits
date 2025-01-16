970403b2933eab880efccdc6a65bb1274ed1afa3
souravgreatest <jainsourav220@gmail.com>
2025-01-16 13:35:32 +0530

    Authentication for signup and signin work by SOurav and Task routing and controlling by Ninad Sasindra

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './src/routes/user.route.js'
+import authRoutes from './src/routes/auth.route.js'
+import tasks from './src/routes/TaskRouter.js'
+import cookieParser from 'cookie-parser';
+dotenv.config();
+
+mongoose.connect(process.env.DB_URL).then(()=>{
+    console.log('Connected to MongoDb');
+}).catch((err)=>{
+    console.log("idhr#######",err);
+})
+const app =express();
+app.use(express.json()); 
+app.use(cookieParser()); 
+
+app.listen(3000,()=>{
+    console.log('Listening on server 3000 !!');
+});
+
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
+app.use('/tasks',tasks);
+
+// middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      message,
+      statusCode,
+    });
+  });

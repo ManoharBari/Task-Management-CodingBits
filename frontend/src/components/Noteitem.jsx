ef8d50ff59a5aea1d826a7f31b7eab4d0c9e5b1c
Manohar Kale <manoharkale5368@gmail.com>
2025-01-18 17:04:50 +0530

    created add note modal

--- frontend/src/components/Noteitem.jsx
+++ frontend/src/components/Noteitem.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import { Pencil, Trash2 } from 'lucide-react'
+import '../styles/notes.css'
 
 function Noteitem() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const note = {
+        title: "Note Title",
+        description: "Note Description",
+        tag: "Note Tag",
+        date: "10.10.2021"
+    }
+    return (
+        <div className="container">
+            <div className="noteItem">
+                <div className="noteContent">
+                    <div className="note-title">
+                        <h3 className="noteTitle">{note.title}</h3>
+                        <div className="action">
+                            <span className='editbtn'> <Pencil size={16} /> </span>
+                            <span className='delbtn'> <Trash2 size={16} /> </span>
+                        </div>
+                    </div>
+                    <p className="notePreview">{note.description}</p>
+                    <div className="noteMeta">
+                        <div className="tags">
+                            <span className="tag">{note.tag}</span>
+                        </div>
+                        <time className="timestamp">{`Created At ${note.date}`}</time>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Noteitem
--- frontend/src/components/Notes.jsx
+++ frontend/src/components/Notes.jsx
@@ -1,60 +1,66 @@
 import React from 'react'
+import Noteitem from './Noteitem'
 import '../styles/notes.css'
 
 function Notes() {
     return (
-        <div>
+        <>
+            <div>
+                <button type="button" className="btn my-2 position-absolute top-1 end-0 mx-3  btn-primary" data-bs-toggle="modal" data-bs-target="#editModal">+ Add
+                </button>
 
-            <button type="button" className="btn my-2 position-absolute top-1 end-0 mx-3  btn-primary" data-bs-toggle="modal" data-bs-target="#editModal">+ Add
-            </button>
+                <h3 className='my-2 mx-4'>Hello Manohar</h3>
 
-            <h3 className='my-2 mx-4'>Hello Manohar</h3>
-
-            <div className="modal fade" id="editModal" tabIndex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="editModalLabel">Add Note</h1>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <form>
-                                <div className="update-editor">
-                                    <input
-                                        type="text"
-                                        placeholder="Note Title"
-                                        className="titleInput"
-                                        name='title'
-
-                                        required
-                                    />
-                                    <textarea
-                                        placeholder="Start typing your note here..."
-                                        className="contentInput"
-                                        name='description'
-
-                                        required
-                                    />
-                                    <div className="tagsSection">
+                <div className="modal fade" id="editModal" tabIndex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h1 className="modal-title fs-5" id="editModalLabel">Add Note</h1>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <form>
+                                    <div className="update-editor">
                                         <input
                                             type="text"
-                                            placeholder="Add a tag"
-                                            className="tagInput"
-                                            name='tag'
+                                            placeholder="Note Title"
+                                            className="titleInput"
+                                            name='title'
 
+                                            required
                                         />
+                                        <textarea
+                                            placeholder="Start typing your note here..."
+                                            className="contentInput"
+                                            name='description'
+
+                                            required
+                                        />
+                                        <div className="tagsSection">
+                                            <input
+                                                type="text"
+                                                placeholder="Add a tag"
+                                                className="tagInput"
+                                                name='tag'
+
+                                            />
+                                        </div>
                                     </div>
-                                </div>
-                            </form>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="action-btn upbtn-close" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="action-btn upbtn-save">Save changes</button>
+                                </form>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="action-btn upbtn-close" data-bs-dismiss="modal">Close</button>
+                                <button type="button" className="action-btn upbtn-save">Save changes</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+            <div>
+                <Noteitem />
+            </div>
+        </>
     )
 }
 
